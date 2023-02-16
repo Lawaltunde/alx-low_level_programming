530e633575348c0c7a85b7b6d2f72c5ae6546dfb
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-16 07:46:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-/*
+/**
+ *main - Entry point
  *
- *  function puts is used
- *
- *  the function return zero value.
- *
- *  
+ *return zero
  */
 int main(void)
-{
-	
+{	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
