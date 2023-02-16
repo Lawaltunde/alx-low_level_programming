24f5ab4c76641d0a76bab6f85a8f60ee82d9eec7
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-16 08:08:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - Entry point
+ * main - where my source code is  written
  *
- *return zero
+ * Return: Always 0 (perfect)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
