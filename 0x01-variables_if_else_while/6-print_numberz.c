d593cff69e8f09c4c6809146fdd1ec19f1018929
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-17 12:39:48 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Printing lowercase alphabet excluding q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
