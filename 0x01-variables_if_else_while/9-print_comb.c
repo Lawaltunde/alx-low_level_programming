dc5cf9fb25eb9bcadefa94b20dea346c6163e302
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-17 14:48:03 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print possible combination of single digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+	for (i = 48; i  < 58; j++)
+	{
+		putchar(i);
+	}
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
