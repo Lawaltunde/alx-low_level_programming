af3f076e42cdedab7c940895bcd6fdfe8dafc5c7
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-21 14:09:27 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,15 @@ int print_last_digit(int i)
 	int b = -1;
 	int c;
 
-	if (i >= 0)
+	if (i < 0)
 	{
+		i *= b;
 		c = i % 10;
 		_putchar(c + '0');
 		return (c);
 	}
 	else
 	{
-		i *= b;
 		c = i % 10;
 		_putchar(c + '0');
 		return (c);
