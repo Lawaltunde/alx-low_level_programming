422005a2a0d3c8bf21fd578dc1b3da2097e06225
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-21 11:01:46 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
 		return (1);
+		_putchar('+');
 	}
 	else
 	if (n == 0)
 	{
-		_putchar(0);
 		return (0);
+		_putchar(0);
 	}
 	else
 	{
-		_putchar('-');
 		return (-1);
+		_putchar('-');
 	}
 }
