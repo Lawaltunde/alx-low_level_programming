c32f91628d3afc746df28c17268985a62137ada3
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-21 16:46:37 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Value 0 (Successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 3; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			printf("%d, ", n);
+		}
+	};
+	return (0);
+}
