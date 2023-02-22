f7a319043c7e5ad9279f67371db4975e6a6337be
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-21 17:01:24 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 3; n < 1024; n++)
+	for (n = 1; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
 			printf("%d\n", n);
 		}
-	};
+	}
 	return (0);
 }
