dcfd5de9fdad93ce0b96844c3673fee527a839cc
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-22 23:22:48 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,23 +14,29 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-	if (month > 2 && day >= 60)
-	{
-		day++;
-	}
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-	if (month == 2 && day == 60)
-	{
+		if (month == 6 && day == 182)
+		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else
-	{
+		}
+		else
+		if (month == 11 && day == 335)
+		{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
+		}
 	}
-	}
+
 }
