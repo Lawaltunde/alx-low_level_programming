addf9c394b07836f86daeae6d10027b4015b52ac
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-23 15:09:36 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,11 @@
 int main(void)
 {
 	int num;
+	num = 1;
 
-	for (num = 1; num <= 100; num++)
+	printf ("%d", num);
+
+	for (num = 2; num <= 100; num++)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
