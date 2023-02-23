cbdc012c11e881655f9118f3f94fc2747e0486a1
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-23 13:33:19 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,25 +7,19 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= 10; i++)
 	{
-		int j;
-
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 			{
 				_putchar((j / 10) + 48);
 				_putchar((j % 10) + 48);
-
 			}
-			else
-				_putchar(j + 48);
 		}
 
 		_putchar('\n');
 	}
-
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * void print_line - function that draws a straight line.
+ * @n: input which is of integer datatype.
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);	
+	}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
