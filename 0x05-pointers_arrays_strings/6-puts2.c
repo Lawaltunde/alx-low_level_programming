3ebad372b21fde02902b3b62748e62d901109f76
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-02-27 14:27:39 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character
+ * @str: input of datatype char.
+ * Return: Void.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+	if (str[i] == '\0')
+	{
+		_putchar('\n');
+		break;
+	}
+	if (str[i] % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
+	i++;
+	}
+}
