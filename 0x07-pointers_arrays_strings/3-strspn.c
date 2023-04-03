d1a346463808129d0fb1dff7be3966044e7e0a8a
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-03 15:05:43 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,29 @@
 
 /**
  * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
+ * @s: destination.
+ * @accept: source.
  * Return: the number of accepted bytes.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, indicator;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 
-		bool = 1;
+		indicator = 1;
 	for (j = 0; *(accept + j) != '\0'; j++)
 	{
 
 		if (*(s + i) == *(accept + j))
 		{
-			bool = 0;
+			indicator = 0;
 			break;
 		}
 	}
-	if (bool == 1)
+	if (indicator == 1)
 		break;
 	}
 	return (i);
