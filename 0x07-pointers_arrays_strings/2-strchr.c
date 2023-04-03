477b54122c93a818c1130b9a2e5263d0502b0971
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-03 08:51:17 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,9 +14,7 @@ char *_strchr(char *s, char c)
 	while (*(s + i) != '\0')
 	{
 	if (*(s + i) == c)
-		return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
+		return (s += i);
 	i++;
 	}
 	return ('\0');
