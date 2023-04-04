3a8a6f66f87701cb07247ba40ddfc88540bd66d8
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-03 22:50:41 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i = 0, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	while (s[i] != '\0')
 	{
-	for (j = 0; *(accept + j) != '\0'; j++)
+	for (j = 0; accept[j] != '\0'; j++)
 	{
 		if (*(s + i) == *(accept + j))
 			return (s + i);
 	}
+	i++;
 	}
 	return  ('\0');
 }
