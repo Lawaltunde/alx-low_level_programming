0c58f9fd8baa1be587ce6e6195ef41ebb1b4e17f
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-06 02:43:31 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _strlen_recursion - function that returns the length of a string.
  * @s: input string
- * Return: number of character contains in a string.
+ * Return:length of the string.
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));	
 	else
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
 }
