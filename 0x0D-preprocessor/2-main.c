e864907df919690ea379e8fa5a61891b4e7d143f
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-18 08:52:45 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- starting of the program that prints the name
+ * of the file it was compiled from, followed
+ * by a new line.
+ * Return: Zero (success).
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
