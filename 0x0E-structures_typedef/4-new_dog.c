875bb4bac0b69b49e0ec0372c9b9f7342527b086
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-17 21:32:22 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,50 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	int i, counterForName = 0, counterForOwner = 0;
 
 	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	if (ptr == NULL || name == NULL || owner == NULL)
+	{
+		free(ptr);
 		return (NULL);
-	ptr->name = name;
+	}
+
+	while (name[counterForName])
+	{
+		;
+		counterForName++;
+	}
+	while (owner[counterForOwner])
+	{
+		;
+		counterForOwner++;
+	}
+
+
+	ptr->name = malloc(counterForName + 1);
+	ptr->owner = malloc(counterForOwner + 1);
+
+	if (!(ptr->name) || !(ptr->owner))
+	{
+		free(ptr->owner);
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < counterForName; i++)
+		ptr->name[i] = name[i];
+	ptr->name[i] = '\0';
+
 	ptr->age = age;
-	ptr->owner = owner;
+
+	for (i = 0; i < counterForOwner; i++)
+		ptr->owner[i] = owner[i];
+	ptr->owner[i] = '\0';
+
 	return (ptr);
+
+
 }
 
