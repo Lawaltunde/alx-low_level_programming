89118c70d61f5b723938a6ba19121bffe183b229
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-19 14:33:44 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
