62f3a342634d81da35690fd5609e56d76180cef7
Lawal Tunde <Lawalolatunde14@gmail.com>
2023-04-30 10:52:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,10 @@ size_t print_list(const list_t *h)
 	while (h != 0)
 	{
 		if (h->str == NULL)
+		{
 		printf("[0] (nil)\n");
+		n++;
+		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,12 +13,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
-
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
