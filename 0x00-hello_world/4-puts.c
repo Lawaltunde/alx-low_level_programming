d0e3391d8a0c29d25a33869d0df3d9c08fbc54b7
Hammed Lawal <lawalolatunde14@gmail.com>
2025-04-30 08:15:37 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - program starting point
+  *
+  *Return: 0 is returned (means successful execution)
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
