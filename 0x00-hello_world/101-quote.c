c8fedc5086e6ccdcb4cc554e078081b2010bdcd0
Hammed Lawal <lawalolatunde14@gmail.com>
2025-04-30 09:33:56 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,8 @@
 
 int main(void)
 {
-        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	char *ms = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ms, 59);
+	return (1);
 }
