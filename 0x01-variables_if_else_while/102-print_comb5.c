8cb8b3544534b80fe9bace8fade8b9d444c56c74
Hammed Lawal <Lawalolatunde14@gmail.com>
2025-05-29 09:06:30 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/*
+ *
+ * main - Execution starts here
+ *
+ * Return: success (0)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for(j = i; j <= 10; j++)
+		{
+			for (k = j; k <= 10; k++)
+			{
+				for (l = k; + l <=10; l++)
+				{
+					putchar(47 + i);
+					putchar(47 + j);
+					putchar(' ');
+					putchar(47 + k);
+					putchar(47 + l);
+					if (i <= 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					else
+					{
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
