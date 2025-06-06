1c5eda7d1bee9e267355beae6835c159d420aa61
HAMMED LAWAL <lawalolatunde14@gmail.com>
2025-06-05 17:39:59 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * jack_bauer -  function that prints every minute of the day
+  *
+  */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(48 + i);
+					_putchar(48 + j);
+					_putchar(':');
+					_putchar(48 + k);
+					_putchar(48 + l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
