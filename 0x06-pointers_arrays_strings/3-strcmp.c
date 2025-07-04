607eca7e375b042018140025e7034dee4bcf5ee5
Hammed Lawal <lawalolatunde14@.com>
2025-07-04 08:29:09 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strncat - function that concatenates two strings
+  *
+  * @s1: pointer to int dtype
+  * @s2: pointer to a int dtype
+  * Return: int (success)
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i= 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
