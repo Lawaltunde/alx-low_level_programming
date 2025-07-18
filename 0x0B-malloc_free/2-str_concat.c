d134db42b51cfb3063ceec47b17cff2e9d44a81f
Hammed Lawal <lawalolatunde@gmail.com>
2025-07-18 08:47:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,29 +12,37 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int i = 0, count = 0, j = 0;
 
-	while (s1[i] != '\0' && s1 != NULL)
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[i] != '\0')
 	{
 		count++;
 		i++;
 	}
 	i = 0;
-	while (s2[i] != '\0' && s2 != NULL)
+	while (s2[i] != '\0')
 	{
 		count++;
 		i++;
 	}
-	ptr = malloc(count * sizeof(char));
+	ptr = malloc((count + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	i = 0;
-	while (s1[i] != '\0' && s1 != NULL)
+	while (s1[i] != '\0')
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0' && s2 != NULL)
+	while (s2[j] != '\0')
 	{
 		ptr[i] = s2[j];
 		j++;
