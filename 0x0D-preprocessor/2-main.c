c563f26ecdda9b54f4c08886b7b69db491cad57f
Hammed Lawal <lawalolatunde@gmail.com>
2025-07-25 13:18:51 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - program entery point
+  * Return: zero means (success)
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
