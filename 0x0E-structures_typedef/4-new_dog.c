86b235ee2279dccfc9eb1baf3f20686f1e0336d2
Hammed Lawal <lawalolatunde14gmail.com>
2025-07-27 11:37:01 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -14,29 +13,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 	char *name_cp, *owner_cp;
+	int i = 0, j = 0, l = 0, m = 0;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
-	name_cp = malloc(strlen(name) + 1);
-	if (name_cp == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	owner_cp = malloc(strlen(owner) + 1);
-	if (owner_cp == NULL)
+	while (name[i++] != '\0')
+		;
+	while (owner[m++] != '\0')
+		;
+	name_cp = malloc(i + 1);
+	owner_cp = malloc(m + 1);
+	if (name_cp == NULL || owner_cp == NULL)
 	{
 		free(name_cp);
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(name_cp, name);
-	strcpy(owner_cp, owner);
-
+	while (name[j] != '\0')
+	{
+		name_cp[j] = name[j];
+		j++;
+	}
+	name_cp[j] = '\0';
+	while (owner[l] != '\0')
+	{
+		owner_cp[l] = owner[l];
+		l++;
+	}
+	owner_cp[l] = '\0';
 	ptr->name = name_cp;
 	ptr->age = age;
 	ptr->owner = owner_cp;
