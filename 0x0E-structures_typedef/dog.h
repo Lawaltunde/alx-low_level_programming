3e2e9e799fb1dc5eadc126e0fea2bef1593c67fc
Hammed Lawal <lawalolatunde@gmail.com>
2025-07-27 04:01:25 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+  * struct dog - type struct containing three variables
+  * @name: pointer to char
+  * @owner: pointer to char
+  * @age: type float
+  * Description: a struct containing three variables: two pointers
+  * char and one float.
+  */
 struct dog{
 	char *name;
 	char *owner;
