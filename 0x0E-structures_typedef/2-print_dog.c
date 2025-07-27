92b35078c008d98478edfad5517375dd00ed600b
Hammed Lawal <lawalolatunde@gmail.com>
2025-07-27 05:17:26 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,16 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			d->name = "(nill)";
+			d->name = "(nil)";
 		}
 		if (d->owner == NULL)
 		{
-			d->owner = "(nill)";
+			d->owner = "(nil)";
 		}
 		printf("Name: %s\n", d->name);
 		if (!d->age)
 		{
-			printf("Age: (nill)\n");
+			printf("Age: (nil)\n");
 		}
 		else
 		{
