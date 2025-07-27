5db2933afabb88a555e17e789db21fd98f88eaba
Hammed Lawal <lawalolatunde14gmail.com>
2025-07-27 10:54:06 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -7,22 +8,37 @@
   * @name: pointer to char
   * @age: float type
   * @owner: pointer to char
-  * Return: pointer to struct dog_t
+  * Return: pointer to struct dog_t, or NULL if failure
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
 	dog_t *ptr;
+	char *name_cp, *owner_cp;
 
 	ptr = malloc(sizeof(dog_t));
-
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	ptr = &dog;
-	ptr->name = name;
+
+	name_cp = malloc(strlen(name) + 1);
+	if (name_cp == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	owner_cp = malloc(strlen(owner) + 1);
+	if (owner_cp == NULL)
+	{
+		free(name_cp);
+		free(ptr);
+		return (NULL);
+	}
+	strcpy(name_cp, name);
+	strcpy(owner_cp, owner);
+
+	ptr->name = name_cp;
 	ptr->age = age;
-	ptr->owner = owner;
+	ptr->owner = owner_cp;
 	return (ptr);
 }
