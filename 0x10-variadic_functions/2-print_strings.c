2cb59afa0547ba3524758e5f61f432ab513b4692
Hammed Lawal <lawalolatunde14gmail.com>
2025-07-31 09:01:18 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: pointer to char
+ * @n: unsigned int typ
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+	char *value;
+
+	va_start(ap, n);
+
+	while (i < n)
+	{
+		value = va_arg(ap, char *);
+		printf("%s", value);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+}
