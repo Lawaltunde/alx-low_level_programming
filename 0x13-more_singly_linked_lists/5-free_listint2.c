f12b421e796c51b9c4a9667fcd32ac44acd90e1e
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-15 08:19:04 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - function that frees a listint_t list.
+  * @head: pointer to pointer to struct
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
