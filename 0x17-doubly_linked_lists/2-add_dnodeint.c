0d312c2e1858054e6d0f4e625ade66d25a71170f
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-24 13:02:24 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 
+	if (!head)
+		return (NULL);
+
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
