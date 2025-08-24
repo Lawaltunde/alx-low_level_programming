30d1e5b8fdb97bb725f4ac79bfd9bdc913389e8d
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-24 14:07:29 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,17 +22,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newnode->n = n;
 	newnode->next = *head;
+	newnode->prev = NULL;
 
-	if (*head == NULL)
-	{
-		*head = newnode;
-		newnode->next = NULL;
-	}
-	else
-	{
-		newnode->prev = (*head)->prev;
-		*head = newnode;
-	}
+	if (*head != NULL)
+		(*head)->prev = newnode;
 
+	*head = newnode;
 	return (newnode);
 }
