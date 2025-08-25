2bbbb75f8343fe7b124d36e8beed00ee0ca27e2c
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-25 06:42:51 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,7 +5,7 @@
   *  the end of a dlistint_t list.
   * @head: pointer to the head pointer
   * @n: data
-  * Return: the address of the new element, 
+  * Return: the address of the new element,
   *  or NULL if it failed
   */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
