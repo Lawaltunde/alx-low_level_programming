c2143f2e8892e2d0e3c7c43af7ae7109078a9899
Hammed Lawal <lawalolatunde14gmail.com>
2025-08-25 08:51:22 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	newnode->n = n;
 	current = *h;
-	while (i < idx)
+	while (i < (idx - 1))
 	{
 		current = current->next;
 		if(current == NULL && i < idx)
