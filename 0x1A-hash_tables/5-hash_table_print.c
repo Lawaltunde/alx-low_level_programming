54a290f5b5fffc8ba2eae65c0d94a59dcd59c468
Hammed Lawal <lawalolatunde14gmail.com>
2025-10-06 09:51:14 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (printed)
 				printf(", ");
-			printf("'%s' : ''%s'", result->value, result->key);
+			printf("'%s' : '%s'", result->key, result->value);
 			printed = 1;
 			result = result->next;
 		}
